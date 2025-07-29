941b1e3fd94e03f048976c13e67088f559becfd1
rafiulhasan1 <rafiulhasan001@gmail.com>
2025-07-29 22:21:16 +0600

    video card part added

--- js/ph-video.js
+++ js/ph-video.js
@@ -7,6 +7,14 @@ const fetchData = () => {
     .catch((error) => console.log(error))
 }
 
+const fetchVideoData = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => localVideo(data.videos))
+    .catch((error) => console.log(error))
+    
+}
+
 const localCatagories = (data) =>{
     // for(const item of data){
     //     console.log(item)        //for of loop
@@ -19,8 +27,45 @@ const localCatagories = (data) =>{
         const button = document.createElement('button')
         button.classList = "btn";
         button.innerText = item.category;
-        categoriesApi.appendChild(button);
+        categoriesApi.append(button);
     })
-} 
+}
+
+
+const localVideo = (data) => {
+    const localVideoId = document.getElementById('local-video');
+    // console.log(data.authors[0].profile_name)
+    data.forEach(item => {
+        // console.log(item.authors[0].profile_name)
+        const div = document.createElement('div');
+        div.classList = "card bg-base-100";
+        div.innerHTML = `
+        
+        <figure class="h-[250px]">
+            <img
+            class="h-full w-full object-cover"
+            src=${item.thumbnail}
+            alt="" />
+        </figure>
+        <div class="flex items-center">
+            <div>
+                <img
+                class="h-10 w-10 object-cover rounded-full"
+                src=${item.authors[0].profile_picture}/>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">${item.title}</h2>
+                <p>${item.authors[0].profile_name}</p>
+                
+                <p>${item.others.views}</p>
+            </div>
+        </div>
+        
+        
+        `
+        localVideoId.append(div);
+    })
+}
 
 fetchData();
+fetchVideoData();
