c8a53a9c5cca1fa984b686325e7a0888c6532bba
rafiulhasan1 <rafiulhasan001@gmail.com>
2025-07-30 19:31:32 +0600

    Verified icon added and load api

--- js/ph-video.js
+++ js/ph-video.js
@@ -31,10 +31,31 @@ const localCatagories = (data) =>{
     })
 }
 
+/**
+ * {
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
+ */
+
 
 const localVideo = (data) => {
     const localVideoId = document.getElementById('local-video');
-    // console.log(data.authors[0].profile_name)
+    console.log(data)
     data.forEach(item => {
         // console.log(item.authors[0].profile_name)
         const div = document.createElement('div');
@@ -55,7 +76,7 @@ const localVideo = (data) => {
             </div>
             <div class="card-body">
                 <h2 class="card-title">${item.title}</h2>
-                <p>${item.authors[0].profile_name}</p>
+                <p>${item.authors[0].profile_name} <span>${item.authors[0].verified ==true ? `<i class="fa-solid fa-certificate" style="color: #74C0FC;"></i>` : ""}</span> </p>
                 
                 <p>${item.others.views}</p>
             </div>
